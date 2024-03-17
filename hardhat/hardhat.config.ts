22c21eccc479f9c250aee57ef5db1ffb9126d69c
Trevor Keith <tk@tkeith.com>
2024-03-17 02:04:16 +0000

    sepolia deployment 1

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -4,6 +4,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -12,6 +13,9 @@ const config: HardhatUserConfig = {
       url: "https://rpc2.sepolia.org",
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
